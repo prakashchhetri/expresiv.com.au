e4551f090371912571e8c96fefdb73ad75ee5d53
Prakash Chhetri <prakash.chhetri@bigtincan.com>
2024-03-02 19:38:48 +1100

    Fix build issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import react from '@astrojs/react';
 
 // https://astro.build/config
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
+
+// https://astro.build/config
 export default defineConfig({
   site: 'https://expresiv.com.au',
-  renderers: ['@astrojs/renderer-react'],
-  integrations: [tailwind(), sitemap()]
+  integrations: [tailwind(), react(), sitemap()]
+  // integrations: [tailwind(), react(), sitemap()] adding sitemap is causing build issues. Need to come back to this later on
+
 });
